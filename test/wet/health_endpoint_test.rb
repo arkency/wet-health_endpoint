69f844bc2231b1e7de88f1ff54b9f0c0441b388d
Szymon Fiedler <fidelski@gmail.com>
2016-02-24 16:25:40 +0100

    Test for handling existing route '/health' in app

--- test/wet/health_endpoint_test.rb
+++ test/wet/health_endpoint_test.rb
@@ -14,5 +14,16 @@ class Wet::HealthEndpointTest < Minitest::Test
     assert_equal 204, last_response.status
     assert_equal '',  last_response.body
   end
+
+  def test_health_check_does_not_override_app_endpoint
+    Rails.application.routes.draw { get '/health', to: proc { [200, {}, ['Yo!']] } }
+
+    get '/health'
+
+    assert_equal 200,   last_response.status
+    assert_equal 'Yo!', last_response.body
+
+    Rails.application.routes.draw {}
+  end
 end
 
